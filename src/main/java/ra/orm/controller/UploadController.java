48045dbfcc056e86935b990d287e10d3049872b5
hunghx <baby10051999@gmail.com>
2024-05-21 19:26:40 +0700

    update

--- src/main/java/ra/orm/controller/UploadController.java
+++ src/main/java/ra/orm/controller/UploadController.java
@@ -10,22 +10,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UploadController {
     private static final  String uploadPath= "C:\\Users\\AD\\IdeaProjects\\mvc-hibernate-config\\src\\main\\webapp\\uploads";
     @PostMapping("/upload")
-    public String doUpload(@RequestParam("file") MultipartFile file, Model model) throws IOException {
+    public String doUpload(@RequestParam("file") List<MultipartFile> files, Model model){
         // đối tượng đại diện cho file upload lên là MultipartFile
         // xử lí upload
         // b1 : lấy ra tên file
-        String fileName =null;
-        if(file.getSize() !=0){
-             fileName= file.getOriginalFilename(); // lấy ra tên file trong thư mục máy tinh
-            FileCopyUtils.copy(file.getBytes(),new File(uploadPath+File.separator+fileName));
-        }
+        List<String> fileNames = new ArrayList<>();
+        files.forEach(file -> {
+            String fileName =null;
+            if(file.getSize() !=0){
+                fileName= file.getOriginalFilename(); // lấy ra tên file trong thư mục máy tinh
+                try {
+                    FileCopyUtils.copy(file.getBytes(),new File(uploadPath+File.separator+fileName));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            fileNames.add(fileName);
+        });
         // upload thành công
-        model.addAttribute("fileName",fileName);
+        model.addAttribute("fileNames",fileNames);
         return "success";
     }
 
