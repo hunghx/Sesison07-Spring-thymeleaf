abbc7c442525e7866bd4a07c5b63bb833daa8f39
hunghx <baby10051999@gmail.com>
2024-05-21 21:13:56 +0700

    update

--- src/main/java/ra/orm/controller/EmployeeController.java
+++ src/main/java/ra/orm/controller/EmployeeController.java
@@ -3,12 +3,15 @@ package ra.orm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ra.orm.entity.Employee;
 import ra.orm.service.IEmployeeService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employees")
@@ -26,7 +29,11 @@ public class EmployeeController {
         return "employee/add";
     }
     @PostMapping("/add")
-    public String doAdd(@ModelAttribute("employee") Employee employee){
+    public String doAdd(@Valid @ModelAttribute("employee") Employee employee, BindingResult result,Model model){
+        if (result.hasErrors()){
+            model.addAttribute("employee",employee);
+            return "employee/add";
+        }
         employeeService.save(employee);
         return "redirect:/employees/list";
     }
--- src/main/java/ra/orm/entity/Employee.java
+++ src/main/java/ra/orm/entity/Employee.java
@@ -4,6 +4,9 @@ import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
@@ -15,6 +18,9 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    // @NotEmpty // mảng , collection , ko được null, size > 0
+    @NotBlank(message = "ko được để trống") //  ko để trống ,""
+//    @NotNull // ko đc mang giá trị null
     private String name;
     @Column(columnDefinition = "date")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
